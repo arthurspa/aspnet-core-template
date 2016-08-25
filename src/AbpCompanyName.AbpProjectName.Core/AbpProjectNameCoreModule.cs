8653afbd4c57bf3df4e89175b136fe79593b4cee
Arthur Silva <arthursilva.ee@gmail.com>
2016-08-25 11:10:31 -0400

    Ported code from module-zero-template .Core project

--- src/AbpCompanyName.AbpProjectName.Core/AbpProjectNameCoreModule.cs
+++ src/AbpCompanyName.AbpProjectName.Core/AbpProjectNameCoreModule.cs
@@ -1,6 +1,11 @@
 ﻿using System.Reflection;
 using Abp.Modules;
 using AbpCompanyName.AbpProjectName.Localization;
+using Abp.Zero.Configuration;
+using AbpCompanyName.AbpProjectName.MultiTenancy;
+using AbpCompanyName.AbpProjectName.Authorization.Roles;
+using AbpCompanyName.AbpProjectName.Users;
+using AbpCompanyName.AbpProjectName.Authorization;
 
 namespace AbpCompanyName.AbpProjectName
 {
@@ -10,7 +15,20 @@ namespace AbpCompanyName.AbpProjectName
         {
             Configuration.Auditing.IsEnabledForAnonymousUsers = true;
 
+            //Declare entity types
+            Configuration.Modules.Zero().EntityTypes.Tenant = typeof(Tenant);
+            Configuration.Modules.Zero().EntityTypes.Role = typeof(Role);
+            Configuration.Modules.Zero().EntityTypes.User = typeof(User);
+
+            //Remove the following line to disable multi-tenancy.
+            Configuration.MultiTenancy.IsEnabled = false;
+
+            //Add/remove localization sources here
             AbpProjectNameLocalizationConfigurer.Configure(Configuration.Localization);
+
+            AppRoleConfig.Configure(Configuration.Modules.Zero().RoleManagement);
+
+            Configuration.Authorization.Providers.Add<AbpProjectNameAuthorizationProvider>();
         }
 
         public override void Initialize()
