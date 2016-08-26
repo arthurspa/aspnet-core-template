d06a9efe9bd362080ba6417c02e30e952c3fd41d
Arthur Silva <arthursilva.ee@gmail.com>
2016-08-26 02:23:03 -0400

    Add DbContext factory to be able to run commands from command line

---
+++ src/AbpCompanyName.AbpProjectName.EntityFramework/EntityFramework/AbpProjectNameDbContextFactory.cs
@@ -0,0 +1,19 @@
+﻿using AbpCompanyName.AbpProjectName.Configuration;
+using AbpCompanyName.AbpProjectName.Web;
+using Microsoft.Extensions.Configuration;
+using System.Data.Entity.Infrastructure;
+
+namespace AbpCompanyName.AbpProjectName.EntityFramework
+{
+    /* This class is needed to run "EntityFramework\Enable-Migrations ..." commands from command line on development. Not used anywhere else */
+    public class AbpProjectNameDbContextFactory : IDbContextFactory<AbpProjectNameDbContext>
+    {
+        public AbpProjectNameDbContext Create()
+        {
+            var configuration = AppConfigurations.Get(WebContentDirectoryFinder.CalculateContentRootFolder());
+            var connectionString = configuration.GetConnectionString(AbpProjectNameConsts.ConnectionStringName);
+
+            return new AbpProjectNameDbContext(connectionString);
+        }
+    }
+}
